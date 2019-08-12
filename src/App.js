934ce8b04688d867c432bda2dfcb58961c2cc3a0
Victor Ezeh <ezehgvictor@gmail.com>
2019-08-12 19:06:08 +0100

    videodetail and video list

--- src/App.js
+++ src/App.js
@@ -6,19 +6,44 @@ import { SearchBar, VideoList, VideoDetail } from './components';
 import { Grid } from '@material-ui/core';
 
 class App extends React.Component {
+
+  state = {
+    videos: [],
+    selectedVideo: null,
+  }
+  
+handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: process.env.REACT_APP_API_KEY,
+        q: searchTerm,
+      }
+    });
+
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+  }
+
+  onVideoSelect = (video) => {
+   this.setState({ selectedVideo: video });
+ }
+
 	render() {
+        const { selectedVideo, videos } = this.state;
+        console.log(selectedVideo)
       return (
         <Grid style={{ justifyContent: 'center' }} container spacing={10}>
           <Grid item xs={11}>
             <Grid container spacing={10}>
               <Grid item xs={12}>
-              	 This is where SearchBar component will go 
+              <SearchBar onFormSubmit={this.handleSubmit} />
               </Grid>
               <Grid item xs={8}>
-              	 This is where VideoDetail component will go
+                 <VideoDetail video={selectedVideo} />
               </Grid>
               <Grid item xs={4}>
-              	 This is where VideoList component will go 
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
               </Grid>
             </Grid>
           </Grid>
